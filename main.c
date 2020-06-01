a44c4332ccba8a5944e0ef43d7dbd79fdaef2213
dusnm <dusan1134@gmail.com>
2020-06-01 22:43:18 +0200

    Remove unneccessary checks

--- main.c
+++ main.c
@@ -22,9 +22,6 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
         case 'm':
             arguments->max_number = atoi(arg);
             break;
-        case ARGP_KEY_ARG:
-            if (state->arg_num > 1) argp_usage(state);
-            break;
         default:
             return ARGP_ERR_UNKNOWN;
     }
